3e07d1bad447462d995185361bd6da4cdf3f3eec
Alex <alex@mail.com>
2013-05-30 22:56:31 +0300

    First commit - initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GaLexi::Application.config.secret_token = '377db70cc181a7f2712a98b01c01fd944f4da42998451ed352796c874b910c0d0db3380a98d9a7d6e72e56176b51cb27a3dfa5f0b2f81fdcb8bbaf10c1c19c9b'
